0838ea1fbfbc574e66bd894c0fbc1d106316f006
苏 <enmoton@qq.com>
2022-09-24 15:43:57 +0800

    add github.com/emirpasic/gods

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
-go 1.18
+go 1.19
+
+require github.com/emirpasic/gods v1.18.1
--- main.go
+++ main.go
@@ -1,7 +1,82 @@
 package main
 
-import "log"
+import "github.com/emirpasic/gods/utils"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// ////////////////////////////////////////////////
+
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+
+	return gcd(b, a%b)
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+
+	return a
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+
+	return a
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+
+	return n
+}
+
+func isPrime(n int) bool {
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func primes(n int) []bool {
+	res := make([]bool, n+1)
+
+	for i := 2; i <= n; i++ {
+		res[i] = true
+	}
+
+	for i := 2; i*i <= n; i++ {
+		if res[i] {
+			for j := 2 * i; j <= n; j += i {
+				res[j] = false
+			}
+		}
+	}
+
+	return res
+}
+
+// ////////////////////////////////////////////
 
 func main() {
-	log.Println("Hello World!")
+	println(utils.ToString("hello world"))
 }

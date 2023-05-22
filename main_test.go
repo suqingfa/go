c0ce3abba2b3d7908904b44b8f42e79e430c617d
苏 <enmoton@qq.com>
2023-05-22 18:32:02 +0800

    -

--- main_test.go
+++ main_test.go
@@ -2,9 +2,16 @@ package leetcode
 
 import (
 	"github.com/emirpasic/gods/utils"
+	"math/rand"
 	"testing"
 )
 
 func Test(t *testing.T) {
 	println(utils.ToString([]string{"hello", "world"}))
+}
+
+func BenchmarkStringAdd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		rand.Int()
+	}
 }

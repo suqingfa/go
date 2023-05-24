c97c6ae038ed61c859aa447f4fa2f92c358fd88c
苏 <enmoton@qq.com>
2023-05-25 00:33:18 +0800

    -

--- main_test.go
+++ main_test.go
@@ -2,11 +2,21 @@ package leetcode
 
 import (
 	"github.com/emirpasic/gods/utils"
+	"reflect"
 	"testing"
 )
 
 func TestToString(t *testing.T) {
 	println(utils.ToString([]string{"hello", "world"}))
+}
+
+func TestSliceEqual(t *testing.T) {
+	a := []int{1, 2, 3, 4}
+	b := []int{1, 2, 3, 4}
+	c := []int{1, 3, 2, 4}
+
+	println(reflect.DeepEqual(a, b))
+	println(reflect.DeepEqual(a, c))
 }
 
 func BenchmarkToString(b *testing.B) {

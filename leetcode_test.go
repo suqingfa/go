3ae1b81064b60767fc01d9556d0a2077c6e8b468
苏 <enmoton@qq.com>
2023-05-31 17:34:03 +0800

    -

--- leetcode_test.go
+++ leetcode_test.go
@@ -1,10 +1,18 @@
 package leetcode
 
 import (
+	"os"
+	"runtime/pprof"
 	"testing"
 )
 
 func TestTable(t *testing.T) {
+	file, _ := os.CreateTemp(os.TempDir(), "cpu.prof")
+	println("cpu.prof:", file.Name())
+	defer file.Close()
+	pprof.StartCPUProfile(file)
+	defer pprof.StopCPUProfile()
+
 	tests := []struct {
 		name   string
 		output int

bb122e87f42e2e39f5d02b5ad4fb5e4134fbe18c
苏 <enmoton@qq.com>
2023-10-14 16:31:45 +0800

    sql

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.21
 
 require (
 	github.com/emirpasic/gods v1.18.1
-	github.com/lib/pq v1.10.9
+	github.com/mattn/go-sqlite3 v1.14.17
 )
--- sql_test.go
+++ sql_test.go
@@ -2,8 +2,8 @@ package leetcode
 
 import (
 	"database/sql"
-	"fmt"
-	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
 	"testing"
 )
 
@@ -14,36 +14,30 @@ func TestSqlDrivers(t *testing.T) {
 }
 
 func TestSql(t *testing.T) {
-	const (
-		host     = "postgres.suqf.top"
-		user     = "postgres"
-		password = "postgres"
-		dbname   = "test"
-	)
-
-	datasource := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s sslmode=disable",
-		host,
-		user,
-		password,
-		dbname,
-	)
-
-	db, _ := sql.Open("postgres", datasource)
+	db, _ := sql.Open("sqlite3", "file::memory:?cache=shared")
 	defer db.Close()
 
-	rows, _ := db.Query("select version()")
+	rows, _ := db.Query("select sqlite_version()")
 
 	for rows.Next() {
 		var version string
 		_ = rows.Scan(&version)
-		println("version:", version)
+		log.Println("sql version:", version)
 	}
 
-	tx, _ := db.Begin()
-	stmt, _ := db.Prepare("insert into factor(factor_id, identity_id, trade_day, value) values ($1, $2, $3, $4)")
+	_, _ = db.Exec("create table user(id int primary key, name varchar(255), password varchar(255))")
 
-	_, _ = stmt.Exec(0, 0, 0, 0.0)
+	tx, _ := db.Begin()
+	stmt, _ := db.Prepare("insert into user(id, name, password) values ($1, $2, $3)")
+
+	_, _ = stmt.Exec(0, "user", "password")
+	_, _ = stmt.Exec(1, "admin", "password")
+
+	query, _ := db.Query("select id, name, password from user")
+	for query.Next() {
+		columns, _ := rows.Columns()
+		log.Println("entity:", columns)
+	}
 
 	_ = tx.Commit()
 }

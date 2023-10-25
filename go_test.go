6d05febfa4bbcc6a1dc08274d7d2694451395a11
苏 <root@suqf.top>
2023-10-25 20:05:15 +0800

    goroutine

---
+++ go_test.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+const N = 1e6
+
+func TestGoroutine(t *testing.T) {
+	c := make(chan int, 10)
+	defer close(c)
+
+	for i := 0; i < N; i++ {
+		go func(c chan int) {
+			time.Sleep(time.Millisecond)
+			c <- 1
+		}(c)
+	}
+
+	value := 0
+	for i := 0; i < N; i++ {
+		value += <-c
+	}
+
+	if value != N {
+		t.Error()
+	}
+	println("value: ", value)
+}
+
+func TestLock(t *testing.T) {
+	mutex := sync.Mutex{}
+	value := 0
+
+	c := make(chan int)
+	defer close(c)
+
+	for i := 0; i < N; i++ {
+		go func(c chan int) {
+			mutex.Lock()
+			defer mutex.Unlock()
+			value++
+			c <- 0
+		}(c)
+	}
+
+	for i := 0; i < N; i++ {
+		<-c
+	}
+
+	if value != N {
+		t.Error()
+	}
+	println("value: ", value)
+}
+
+func TestAtomic(t *testing.T) {
+	value := atomic.Int64{}
+
+	c := make(chan int)
+	defer close(c)
+
+	for i := 0; i < N; i++ {
+		go func(c chan int) {
+			value.Add(1)
+			c <- 0
+		}(c)
+	}
+
+	for i := 0; i < N; i++ {
+		<-c
+	}
+
+	if value.Load() != N {
+		t.Error()
+	}
+	println("value: ", value.Load())
+}
--- main_test.go
+++ main_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/emirpasic/gods/queues/priorityqueue"
 	"github.com/emirpasic/gods/utils"
 	"reflect"
-	"sync"
 	"testing"
 	"time"
 )
@@ -81,52 +80,6 @@ func TestTimeParse(t *testing.T) {
 		return
 	}
 	println(utils.ToString(parse))
-}
-
-func TestGoroutine(t *testing.T) {
-	f := func(i int, c chan int) {
-		time.Sleep(200_000_000)
-		c <- i
-	}
-
-	const N = 1000
-
-	c := make(chan int, 10)
-
-	for i := 0; i < N; i++ {
-		go f(i, c)
-	}
-
-	sum := 0
-	for i := 0; i < N; i++ {
-		sum += <-c
-	}
-
-	println("sum: ", sum)
-	close(c)
-}
-
-func TestLock(t *testing.T) {
-	mutex := sync.Mutex{}
-	value := 0
-
-	const N = 100_000
-	c := make(chan int)
-
-	for i := 0; i < N; i++ {
-		go func(c chan int) {
-			mutex.Lock()
-			defer mutex.Unlock()
-			value++
-			c <- 0
-		}(c)
-	}
-
-	for i := 0; i < N; i++ {
-		<-c
-	}
-
-	println("value: ", value)
 }
 
 func BenchmarkToString(b *testing.B) {

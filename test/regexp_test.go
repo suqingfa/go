605154b5a878f1898a6463a9790c1d7d86eef144
suqf <root@suqf.top>
2024-04-16 22:40:36 +0800

    regexp

---
+++ test/regexp_test.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"regexp"
+	"testing"
+)
+
+func TestMatch(t *testing.T) {
+	matched, err := regexp.MatchString(`^Go\w*123$`, "Golang123")
+	assert.Nil(t, err)
+	assert.True(t, matched)
+}
+
+func TestFindString(t *testing.T) {
+	reg := regexp.MustCompile(`[+-]?\d*x?`)
+	assert.Equal(t, []string{"x", "+5", "-3", "+x"}, reg.FindAllString("x+5-3+x", -1))
+}

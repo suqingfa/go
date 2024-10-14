4530e007988db5dfd9fb1efa26956f5dff2950be
suqf <root@suqf.top>
2024-10-14 18:05:08 +0800

    reflect

---
+++ test/reflect_test.go
@@ -0,0 +1,25 @@
+package test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+)
+
+func TestTypeOf(t *testing.T) {
+	i := 10
+	tp := reflect.TypeOf(i)
+	assert.Equal(t, reflect.Int, tp.Kind())
+
+	value := reflect.New(tp)
+	elem := value.Elem()
+	elem.SetInt(20)
+	assert.Equal(t, 20, elem.Interface())
+}
+
+func TestValueOf(t *testing.T) {
+	i := 10
+	v := reflect.ValueOf(&i)
+	v.Elem().SetInt(20)
+	assert.Equal(t, 20, i)
+}

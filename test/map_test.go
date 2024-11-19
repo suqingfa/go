79617626cb09a0082336816085aef427ba6fc9d7
suqf <root@suqf.top>
2024-11-19 21:30:51 +0800

    map benchmark

---
+++ test/map_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"testing"
+)
+
+func BenchmarkSliceAccess(b *testing.B) {
+	arr := make([]int, 1e5)
+	for i := 0; i < b.N; i++ {
+		arr[i%1e5] = i
+	}
+}
+
+func BenchmarkMapFast64(b *testing.B) {
+	m := make(map[int]int)
+	for i := 0; i < b.N; i++ {
+		m[i] = i
+	}
+	b.Log(len(m))
+}
+
+func BenchmarkMap(b *testing.B) {
+	m := make(map[float64]int)
+	for i := 0; i < b.N; i++ {
+		m[float64(i)] = i
+	}
+	b.Log(len(m))
+}

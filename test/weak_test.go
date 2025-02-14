aee89f18be0cb9a30ffbfd54fec89b319efab9f4
suqf <root@suqf.top>
2025-02-14 23:40:16 +0800

    weak reference

---
+++ test/weak_test.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"runtime"
+	"testing"
+	"weak"
+)
+
+type T struct {
+	// N.B. This must contain a pointer, otherwise the weak handle might get placed
+	// in a tiny block making the tests in this package flaky.
+	t *T
+	a int
+}
+
+func TestWeak(t *testing.T) {
+	p := weak.Make(new(T))
+	assert.NotNil(t, p.Value())
+
+	runtime.GC()
+	assert.Nil(t, p.Value())
+}

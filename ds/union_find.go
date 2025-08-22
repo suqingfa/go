28330d7592321c65b49775f1797834a55f675f26
suqf <root@suqf.top>
2025-08-22 18:24:24 +0800

    Union Find

--- ds/union_find.go
+++ ds/union_find.go
@@ -1,15 +1,35 @@
 package ds
 
 // UnionFind 并查集
-type UnionFind[T comparable] struct {
+type UnionFind[T comparable] interface {
+
+	// Find 查找 t 的代表
+	Find(t T) T
+
+	// Union 将 a 与 b 合并，返回 true 为当前操作将两个集合合并在一起
+	Union(a, b T) bool
+
+	// IsConnected 测试 a 与 b 是否在同一个集合
+	IsConnected(a, b T) bool
+}
+
+func NewUnionFind[T comparable]() UnionFind[T] {
+	return &unionFindUseMap[T]{make(map[T]T)}
+}
+
+func NewUnionFindInt(size int) UnionFind[int] {
+	arr := make([]int, size)
+	for i := range size {
+		arr[i] = i
+	}
+	return &unionFindUseArray{arr}
+}
+
+type unionFindUseMap[T comparable] struct {
 	leader map[T]T
 }
 
-func NewUnionFind[T comparable]() *UnionFind[T] {
-	return &UnionFind[T]{make(map[T]T)}
-}
-
-func (uf *UnionFind[T]) Find(t T) T {
+func (uf *unionFindUseMap[T]) Find(t T) T {
 	if _, ok := uf.leader[t]; !ok {
 		return t
 	}
@@ -18,11 +38,11 @@ func (uf *UnionFind[T]) Find(t T) T {
 	return uf.leader[t]
 }
 
-func (uf *UnionFind[T]) IsConnected(a T, b T) bool {
+func (uf *unionFindUseMap[T]) IsConnected(a T, b T) bool {
 	return uf.Find(a) == uf.Find(b)
 }
 
-func (uf *UnionFind[T]) Union(a T, b T) bool {
+func (uf *unionFindUseMap[T]) Union(a T, b T) bool {
 	la, lb := uf.Find(a), uf.Find(b)
 	if la == lb {
 		return false
@@ -30,4 +50,33 @@ func (uf *UnionFind[T]) Union(a T, b T) bool {
 
 	uf.leader[lb] = la
 	return true
+}
+
+type unionFindUseArray struct {
+	arr []int
+}
+
+func (uf *unionFindUseArray) Find(t int) int {
+	res := uf.arr[t]
+	for res != uf.arr[res] {
+		res = uf.arr[res]
+	}
+
+	uf.arr[t] = res
+
+	return res
+}
+
+func (uf *unionFindUseArray) Union(a, b int) bool {
+	la, lb := uf.Find(a), uf.Find(b)
+	if la == lb {
+		return false
+	}
+
+	uf.arr[lb] = la
+	return true
+}
+
+func (uf *unionFindUseArray) IsConnected(a, b int) bool {
+	return uf.Find(a) == uf.Find(b)
 }
--- ds/union_find_test.go
+++ ds/union_find_test.go
@@ -17,3 +17,15 @@ func TestUnionFind(t *testing.T) {
 	assert.True(t, uf.IsConnected(1, 2))
 	assert.False(t, uf.IsConnected(1, 3))
 }
+
+func TestUnionFindUseIntArray(t *testing.T) {
+	uf := NewUnionFindInt(4)
+
+	assert.True(t, uf.Union(1, 2))
+	assert.False(t, uf.Union(1, 2))
+
+	assert.Equal(t, 1, uf.Find(2))
+
+	assert.True(t, uf.IsConnected(1, 2))
+	assert.False(t, uf.IsConnected(1, 3))
+}

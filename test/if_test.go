b3cd1b27cc8c968f6173dbcb16fdf2dc3d849b8c
suqf <root@suqf.top>
2025-09-20 20:54:17 +0800

    if test

---
+++ test/if_test.go
@@ -0,0 +1,50 @@
+package test
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func BenchmarkIfSorted(b *testing.B) {
+	arr := make([]int, 1000)
+	for i := range arr {
+		arr[i] = i
+	}
+
+	b.ResetTimer()
+
+	res := 0
+	for i := 0; i < b.N; i++ {
+		for _, j := range arr {
+			if j%10 < 1 {
+				res++
+			} else {
+				res--
+			}
+		}
+	}
+}
+
+func BenchmarkIfNotSorted(b *testing.B) {
+	arr := make([]int, 1000)
+	for i := range arr {
+		arr[i] = i
+	}
+
+	rand.Shuffle(len(arr), func(i, j int) {
+		arr[i], arr[j] = arr[j], arr[i]
+	})
+
+	b.ResetTimer()
+
+	res := 0
+	for i := 0; i < b.N; i++ {
+		for _, j := range arr {
+			if j%10 >= 1 {
+				res--
+			} else {
+				res++
+			}
+		}
+	}
+}

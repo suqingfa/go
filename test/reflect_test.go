9ecc6f8a09b573e1e32c9e13e00356645485702a
suqf <root@suqf.top>
2026-05-30 19:49:05 +0800

    reflect test

--- test/reflect_test.go
+++ test/reflect_test.go
@@ -24,3 +24,30 @@ func TestValueOf(t *testing.T) {
 	v.Elem().SetInt(20)
 	assert.Equal(t, 20, i)
 }
+
+func TestCreateByType(t *testing.T) {
+	{
+		tp := reflect.TypeFor[int]()
+		ptr := reflect.New(tp)
+		ptr.Elem().SetInt(100)
+		assert.Equal(t, 100, ptr.Elem().Interface())
+	}
+
+	{
+		tp := reflect.TypeFor[[]int]()
+		value := reflect.MakeSlice(tp, 3, 3)
+		value.Index(0).SetInt(0)
+		value.Index(1).SetInt(1)
+		value.Index(2).SetInt(2)
+		assert.Equal(t, []int{0, 1, 2}, value.Interface())
+	}
+
+	{
+		tp := reflect.TypeFor[map[string]int]()
+		value := reflect.MakeMap(tp)
+		value.SetMapIndex(reflect.ValueOf("a"), reflect.ValueOf(1))
+		value.SetMapIndex(reflect.ValueOf("b"), reflect.ValueOf(2))
+		value.SetMapIndex(reflect.ValueOf("c"), reflect.ValueOf(3))
+		assert.Equal(t, map[string]int{"a": 1, "b": 2, "c": 3}, value.Interface())
+	}
+}
